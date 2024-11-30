fd84c7d683906a43cefb36d526f286f23d06a757
Mohd Hamza <mohdhamza.me@gmail.com>
2024-11-30 11:43:37 +0530

    updating the tinymce editor

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,19 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
-import "./RTE.css"
+import "./RTE.css";
 import conf from "../conf/conf";
 import { useAskContext } from "../context/AskContext";
 
-const RTE = ({
-  name,
-  control,
-  defaultValue = "",
-}) => {
-
-  const {
-    setnotificationPopMsg,
-    setNotificationPopMsgNature } = useAskContext()
+const RTE = ({ name, control, defaultValue = "" }) => {
+  const { setnotificationPopMsg, setNotificationPopMsgNature } =
+    useAskContext();
 
   return (
     <div className="w-full">
@@ -29,11 +23,26 @@ const RTE = ({
               height: 500,
               autoresize_max_height: 800,
               menubar: false,
-              plugins: ["lists", "image", "wordcount"],
+              plugins:
+                "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount linkchecker",
               toolbar:
-                "undo redo | image | bold italic forecolor backcolor  | alignleft aligncenter alignright alignjustify | outdent indent |removeformat",
+                "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | anchor | removeformat | custom_anchor | codesample",
               content_style:
-                "body { font-family:Helvetica,Arial,sans-serif; font-size:18px }",
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+              codesample_languages: [
+                { text: "HTML/XML", value: "markup" },
+                { text: "JavaScript", value: "javascript" },
+                { text: "JSX", value: "jsx" },
+                { text: "TypeScript", value: "typescript" },
+                { text: "TSX", value: "tsx" },
+                { text: "CSS", value: "css" },
+                { text: "SCSS", value: "scss" },
+                { text: "Python", value: "python" },
+                { text: "Java", value: "java" },
+                { text: "C", value: "c" },
+                { text: "C#", value: "csharp" },
+                { text: "C++", value: "cpp" },
+              ],
               file_picker_callback: function (callback, value, meta) {
                 if (meta.filetype === "image") {
                   const editor = tinymce.activeEditor;
@@ -47,9 +56,11 @@ const RTE = ({
                     const file = input.files[0];
                     const MAX_FILE_SIZE = 1 * 1024 * 1024;
                     if (file.size > MAX_FILE_SIZE) {
-                      setNotificationPopMsgNature((prev) => false)
-                      setnotificationPopMsg((prev) => "Image Must be Less then and Equal to 1 MB")
-                      return
+                      setNotificationPopMsgNature((prev) => false);
+                      setnotificationPopMsg(
+                        (prev) => "Image Must be Less then and Equal to 1 MB"
+                      );
+                      return;
                     }
                     const reader = new FileReader();
                     reader.onload = function () {
@@ -64,7 +75,6 @@ const RTE = ({
                   input.click();
                 }
               },
-
             }}
             onEditorChange={(content) => {
               onChange(content);
