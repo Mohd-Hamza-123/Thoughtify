32b20963ca9fb397953edd4af0957e59b43f5bb0
Mohd Hamza <mohdhamza.me@gmail.com>
2025-04-18 23:04:40 +0530

    refactor - updating docs

--- src/components/UserCircle.jsx
+++ src/components/UserCircle.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { useAskContext } from "../context/AskContext";
-import NoProfile from '../assets/NoProfile.png'
-const UserCircle = () => {
-  const { myUserProfile } = useAskContext()
+import NoProfile from "../assets/NoProfile.png";
+const UserCircle = ({ ...props }) => {
+  const { myUserProfile } = useAskContext();
   return (
-    <div id="upperNavbar_svg_div">
-      <img src={myUserProfile?.profileImgURL}
-        onError={(e) => { e.target.src = NoProfile }}
+    <div {...props} id="upperNavbar_svg_div">
+      <img
+        src={myUserProfile?.profileImgURL}
+        onError={(e) => {
+          e.target.src = NoProfile;
+        }}
       />
     </div>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -38,5 +38,6 @@ import Logo from "./Logo";
 import SvgIcons from "./SvgIcons";
 import UpperNavigationBarNotification from "./NavBar/UpperNavigationBarNotification";
 import UpperNavigationBarSearch from "./NavBar/UpperNavigationBarSearch";
-export { UpperNavigationBar, LowerNavigationBar, NavBar, AskQue, SideBar, Input, Button, Container, Signup, Login, UserCircle, AuthLayout, RTE, PostCard, TextArea, ViewPost, Chat, ChatRTE, MyProfile, ProfileSummary, Favourite, Questions, Opinions, EditProfile, Overlay, PersonalChat, HorizontalLine, HomeRight, BrowseQuestions, Feedback, Spinner, ChatInProfile, Loader, NotificationPop, SecondLoader,DarkModeToggle , Logo , SvgIcons,UpperNavigationBarNotification, UpperNavigationBarSearch}
+import UpperNavigationBarBtns from "./NavBar/UpperNavigationBarBtns";
+export { UpperNavigationBar, LowerNavigationBar, NavBar, AskQue, SideBar, Input, Button, Container, Signup, Login, UserCircle, AuthLayout, RTE, PostCard, TextArea, ViewPost, Chat, ChatRTE, MyProfile, ProfileSummary, Favourite, Questions, Opinions, EditProfile, Overlay, PersonalChat, HorizontalLine, HomeRight, BrowseQuestions, Feedback, Spinner, ChatInProfile, Loader, NotificationPop, SecondLoader,DarkModeToggle , Logo , SvgIcons,UpperNavigationBarNotification, UpperNavigationBarSearch,UpperNavigationBarBtns}
 
